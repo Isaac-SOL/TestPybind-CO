926aba9c00d484bdd9e78931ce712ec9ddda3356
Isaac-SOL <pi.pou.let@hotmail.fr>
2021-01-21 22:13:55 +0100

    Extraction de résultats Numpy

--- TestPybind.cpp
+++ TestPybind.cpp
@@ -1,12 +1,27 @@
-﻿// TestPybind.cpp : définit le point d'entrée de l'application.
-//
+﻿// TestPybind : Test de fonctionnement de l'embed Python dans C++
+// Par Pierre Cuquel
 
 #include "TestPybind.h"
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+#include <pybind11/numpy.h>
 
-using namespace std;
+namespace py = pybind11;
+using namespace py::literals;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello from C++" << std::endl;
+	py::scoped_interpreter guard{};
+
+	auto locals = py::dict("res"_a = "test");
+	py::exec(R"pydelim(
+		import numpy as np
+		res = np.array([[1, 2, 3], [4, 5, 6]])
+	)pydelim", py::globals(), locals);
+	for (auto& item : locals["res"])
+		for (auto& item2 : item)
+			std::cout << "res = " << item2.cast<int>() << std::endl;
+
 	return 0;
 }
