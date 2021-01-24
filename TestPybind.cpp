23d18b4e1869ddd30907233370ec84a02be662dd
Isaac-SOL <pi.pou.let@hotmail.fr>
2021-01-24 19:44:37 +0100

    Interfaçage Numpy et ajout de classes

--- TestPybind.cpp
+++ TestPybind.cpp
@@ -3,33 +3,162 @@
 
 #include "TestPybind.h"
 #include <fstream>
+#include <vector>
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
 #include <pybind11/numpy.h>
+#include <random>
 
 namespace py = pybind11;
 using namespace py::literals;
 
+typedef std::vector<std::pair<float, float>> samples_t;
+
+py::array_t<float> toNumpy(samples_t distrib) {
+	py::array_t<float> samples{ static_cast<int>(distrib.size() * 2), (float*)distrib.data() };
+	samples.resize({ static_cast<int>(distrib.size()), 2 });
+	return samples;
+}
+
+samples_t fromNumpy(py::array_t<float> samples) {
+	samples_t distrib;
+	distrib.reserve(samples.size()); // à voir si c'est vraiment la bonne taille ou s'il faut faire *2
+	for (auto& col : samples) {
+		std::vector<float> tmp;
+		for (auto& item : col)
+			tmp.push_back(item.cast<float>());
+		distrib.push_back({ tmp[0], tmp[1] });
+	}
+	return distrib;
+}
+
+class RealNVPwrapper {
+public:
+	RealNVPwrapper(int sampleDims = 2, int hiddenDims = 256, int couplingLayers = 8, float learningRate = 1e-4f, bool allowGPU = true) :
+		scope(py::module::import("__main__").attr("__dict__")) // Je comprends pas encore exactement ce que ça fait
+	{
+		try {
+			scope["sample_dims"] = sampleDims;
+			scope["hidden_dims"] = hiddenDims;
+			scope["coupling_layers"] = couplingLayers;
+			scope["learning_rate"] = learningRate;
+			scope["allow_gpu"] = allowGPU;
+			py::eval_file("../../../pysrc/tutoRealNVP.py", scope);
+		}
+		catch (py::error_already_set& err) {
+			std::cerr << "Exception Python levée :" << std::endl;
+			std::cerr << err.what() << std::endl;
+		}
+	}
+
+	void train(int batchSize = 256, int epochs = 20, float validationSplit = 0.2f) {
+		try {
+			py::eval("model.fit(normalized_data, verbose=2, batch_size=" + std::to_string(batchSize)
+				+ ", epochs=" + std::to_string(epochs)
+				+ ", validation_split=" + std::to_string(validationSplit)
+				+ ")", scope);
+		}
+		catch (py::error_already_set& err) {
+			std::cerr << "Exception Python levée :" << std::endl;
+			std::cerr << err.what() << std::endl;
+		}
+	}
+
+	samples_t forward(samples_t& samples) {
+		try {
+			scope["samples"] = toNumpy(samples);
+			auto warped = py::eval("model.forward(samples)", scope);
+			return fromNumpy(warped);
+		}
+		catch (py::error_already_set& err) {
+			std::cerr << "Exception Python levée :" << std::endl;
+			std::cerr << err.what() << std::endl;
+		}
+		return {};
+	}
+
+	samples_t inverse(samples_t& samples) {
+		try {
+			scope["samples"] = toNumpy(samples);
+			auto warped = py::eval("model.inverse(samples)", scope);
+			return fromNumpy(warped);
+		}
+		catch (py::error_already_set& err) {
+			std::cerr << "Exception Python levée :" << std::endl;
+			std::cerr << err.what() << std::endl;
+		}
+		return {};
+	}
+
+private:
+	py::scoped_interpreter guard; // RAII durée de vie de l'interpréteur
+	py::dict scope;
+};
+
+class UniformDistribution {
+public:
+	UniformDistribution(float low, float high) :
+		dist(low, high)
+	{}
+
+	samples_t generate(int size) {
+		samples_t distrib;
+		for (int i = 0; i < size; i++)
+			distrib.push_back({ dist(gen), dist(gen) });
+		return distrib;
+	}
+
+private:
+	std::default_random_engine gen;
+	std::uniform_real_distribution<float> dist;
+};
+
 int main()
+{
+	std::cout << "Step 0 : Loading Tensorflow and RealNVP" << std::endl;
+	RealNVPwrapper realNVP{ 2, 256, 8, 1e-4, false };
+
+	std::cout << "Step 1 : Training RealNVP" << std::endl;
+	realNVP.train();
+
+	std::cout << "Step 2 : Loading an example Numpy array" << std::endl;
+	UniformDistribution dist{ 0, 1 };
+	samples_t samples = dist.generate(200);
+
+	std::cout << "Step 3 : Warping the samples using RealNVP and catching the result" << std::endl;
+	auto warped = realNVP.forward(samples);
+
+	std::cout << "Step 4 : Reading the results" << std::endl;
+	for (auto& sample : warped) {
+		std::cout << sample.first << ", " << sample.second << std::endl;
+	}
+
+	std::system("pause");
+	return 0;
+}
+
+/*
+int main_old()
 {
 	// Démarrage de l'interpréteur Python
 	py::scoped_interpreter guard{};
 
 	// Lecture du fichier Python
-	std::ifstream ifs("../../../pysrc/tutoRealNVP.py");
-	std::string pycode((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+	//std::ifstream ifs("../../../pysrc/tutoRealNVP.py");
+	//std::string pycode((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
 
-	auto locals = py::dict("res"_a = "test");
+	py::object scope = py::module_::import("__main__").attr("__dict__");
 	try {
-		py::exec(pycode, py::globals(), locals);
+		py::eval_file("../../../pysrc/tutoRealNVP.py", scope);
 	}
 	catch (py::error_already_set& err) {
 		std::cerr << "Exception Python levée :" << std::endl;
 		std::cerr << err.what() << std::endl;
 	}
-	for (auto& item : locals["warped_samples"])
+	for (auto& item : scope["warped_samples"])
 		for (auto& item2 : item)
-			std::cout << "res = " << item2.cast<int>() << std::endl;
+			std::cout << "res = " << item2.cast<float>() << std::endl;
 
 	return 0;
 }
+*/
--- pysrc/tutoRealNVP.py
+++ pysrc/tutoRealNVP.py
@@ -1,4 +1,18 @@
+# -*- coding: utf-8 -*-
 # Test Real NVP
+
+# Valeurs par défaut
+#sample_dims = 2
+#hidden_dims = 256
+#coupling_layers = 8
+#learning_rate = 1e-4
+#allow_gpu = True
+
+# Ces lignes font en sorte de n'utiliser que le CPU, même si le GPU est disponible.
+if not allow_gpu:
+    import os
+    os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
+
 
 import tensorflow as tf
 from tensorflow import keras
@@ -8,9 +22,8 @@ from sklearn.datasets import make_moons
 import numpy as np
 import tensorflow_probability as tfp
 
-print("test")
-
-# Loading data
+# Loading example data
+# Note : ne marche que si sample_dims = 2
 
 data = make_moons(3000, noise=0.05)[0].astype("float32")
 norm = layers.experimental.preprocessing.Normalization()
@@ -18,13 +31,9 @@ norm.adapt(data)
 normalized_data = norm(data)
 
 # Defining coupling layers
-
 # Creating a custom layer with keras API.
-output_dim = 256
-reg = 0.01
 
-
-def Coupling(input_shape):
+def Coupling(input_shape, output_dim, reg=0.01):
     input = keras.layers.Input(shape=input_shape)
 
     t_layer_1 = keras.layers.Dense(
@@ -64,85 +73,77 @@ def Coupling(input_shape):
 # Real NVP class
 
 class RealNVP(keras.Model):
-  def __init__(self, num_coupling_layers):
-    super(RealNVP, self).__init__()
+    def __init__(self, num_coupling_layers):
+        super(RealNVP, self).__init__()
+        
+        self.num_coupling_layers = num_coupling_layers
 
-    self.num_coupling_layers = num_coupling_layers
+        # Distribution of the latent space.
+        self.distribution = tfp.distributions.MultivariateNormalDiag(
+            loc=[0.0,0.0], scale_diag=[1.0,1.0]
+        )
+        self.masks = np.array(
+            [[0, 1], [1, 0]] * (num_coupling_layers // 2), dtype="float32"
+        )
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.layers_list = [Coupling(sample_dims, hidden_dims) for i in range(num_coupling_layers)]
 
-    # Distribution of the latent space.
-    self.distribution = tfp.distributions.MultivariateNormalDiag(
-        loc=[0.0,0.0], scale_diag=[1.0,1.0]
-    )
-    self.masks = np.array(
-        [[0, 1], [1, 0]] * (num_coupling_layers // 2), dtype="float32"
-    )
-    self.loss_tracker = keras.metrics.Mean(name="loss")
-    self.layers_list = [Coupling(2) for i in range(num_coupling_layers)]
-  
-  @property
-  def metrics(self):
-      """List of the model's metrics.
-      We make sure the loss tracker is listed as part of `model.metrics`
-      so that `fit()` and `evaluate()` are able to `reset()` the loss tracker
-      at the start of each epoch and at the start of an `evaluate()` call.
-      """
-      return [self.loss_tracker]
+    @property
+    def metrics(self):
+        # List of the model's metrics.
+        # We make sure the loss tracker is listed as part of `model.metrics`
+        # so that `fit()` and `evaluate()` are able to `reset()` the loss tracker
+        # at the start of each epoch and at the start of an `evaluate()` call.
+        return [self.loss_tracker]
 
-  def call(self, x, training=True):
-    log_det_inv = 0
-    direction = 1
-    if training:
-      direction = -1
-    for i in range(self.num_coupling_layers)[::direction]:
-      x_masked = x * self.masks[i]
-      reversed_mask = 1 - self.masks[i]
-      s,t = self.layers_list[i](x_masked)
-      s *= reversed_mask
-      t *= reversed_mask
-      gate = (direction - 1) / 2
-      x = (
-          reversed_mask
-           * (x * tf.exp(direction * s) + direction * t * tf.exp(gate * s))
-           + x_masked
-      )
-      log_det_inv += gate * tf.reduce_sum(s,[1])
+    def call(self, x, training=True):
+        log_det_inv = 0
+        direction = 1
+        if training:
+            direction = -1
+        for i in range(self.num_coupling_layers)[::direction]:
+            x_masked = x * self.masks[i]
+            reversed_mask = 1 - self.masks[i]
+            s,t = self.layers_list[i](x_masked)
+            s *= reversed_mask
+            t *= reversed_mask
+            gate = (direction - 1) / 2
+            x = (
+                reversed_mask
+                    * (x * tf.exp(direction * s) + direction * t * tf.exp(gate * s))
+                    + x_masked
+            )
+            log_det_inv += gate * tf.reduce_sum(s,[1])
     
-    return x, log_det_inv
+        return x, log_det_inv
 
-  def log_loss(self, x):
-    y, logdet = self(x);
-    log_likelihood = self.distribution.log_prob(y) + logdet
-    return -tf.reduce_mean(log_likelihood)
+    def log_loss(self, x):
+        y, logdet = self(x);
+        log_likelihood = self.distribution.log_prob(y) + logdet
+        return -tf.reduce_mean(log_likelihood)
 
-  def train_step(self, data):
-    with tf.GradientTape() as tape:
-      loss = self.log_loss(data)
-    g = tape.gradient(loss, self.trainable_variables)
-    self.optimizer.apply_gradients(zip(g, self.trainable_variables))
-    self.loss_tracker.update_state(loss)
-    return {"loss": self.loss_tracker.result()}
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            loss = self.log_loss(data)
+        g = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(g, self.trainable_variables))
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
 
-  def test_step(self, data):
-    loss = self.log_loss(data);
-    self.loss_tracker.update_state(loss)
-    return {"loss": self.loss_tracker.result()}
+    def test_step(self, data):
+        loss = self.log_loss(data);
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
 
-  def forward(self, x):
-    y, _ = self(x);
-    return y
+    def forward(self, x):
+        y, _ = self(x);
+        return y
 
-  def inverse(self, y):
-    x, _ = self(y, training=False)
-    return x
+    def inverse(self, y):
+        x, _ = self(y, training=False)
+        return x
 
 # Model Training
 
-model = RealNVP(num_coupling_layers=12)
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001))
-history = model.fit(normalized_data, batch_size=256, epochs=100, verbose=2, validation_split=0.2)
-
-# Performance validation
-
-# From latent space to data.
-samples = model.distribution.sample(100)
-warped_samples = model.inverse(samples)
+model = RealNVP(coupling_layers)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate))
